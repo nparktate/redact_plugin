0ec39dbb5a29021abf3e753242db4ac46bab294a
Nick <57370896+nparktate@users.noreply.github.com>
2023-03-14 10:48:58 -0400

    Version: 0.12
    - Added slider for vertical scale
    - Removed redundant code

--- RedactScript.jsx
+++ RedactScript.jsx
@@ -11,16 +11,18 @@ var shapeLayer = app.project.activeItem.layers.addShape();
 var rectPath = shapeLayer.property("Contents").addProperty("ADBE Vector Shape - Rect");
 
 // Set the rectangle path to the bounding box of the text layer
-rectPath.property("Size").expression = "var t = thisComp.layer('" + textLayer.name + "'); [t.sourceRectAtTime(time,false).width, t.sourceRectAtTime(time,false).height];";
+rectPath.property("Size").expression = "var t = thisComp.layer('" + textLayer.name + "'); [t.sourceRectAtTime(time,false).width, t.sourceRectAtTime(time,false).height*thisComp.layer('" + shapeLayer.name + "').effect('Vertical Scale')('Slider')/100];";
 rectPath.property("Position").setValue([boundingBox.left + boundingBox.width/2, boundingBox.top + boundingBox.height/2]);
 
-// Set the stroke and fill colors of the rectangle
-var stroke = shapeLayer.property("Contents").addProperty("ADBE Vector Graphic - Stroke");
-stroke.property("Color").setValue([1, 1, 1]); // white stroke
-stroke.property("Stroke Width").setValue(2);
-
+// Set the fill color of the rectangle
 var fill = shapeLayer.property("Contents").addProperty("ADBE Vector Graphic - Fill");
 fill.property("Color").setValue([0, 0, 0]); // black fill
+fill.property("Opacity").setValue(50); // 50% opacity
+
+// Add a slider control to the shape layer for vertical scale
+var verticalScale = shapeLayer.Effects.addProperty("ADBE Slider Control");
+verticalScale.name = "Vertical Scale";
+verticalScale.property("Slider").setValue(100);
 
 // Set the anchor point of the text layer to the center of the bounding box
 textLayer.transform.anchorPoint.expression = "var s = sourceRectAtTime(); [s.left+s.width/2, s.top+s.height/2];";
@@ -34,7 +36,3 @@ shapeLayer.parent = textLayer;
 // Set the position of the shape layer to the text layer's anchor point
 shapeLayer.transform.position.expression = "var t = thisComp.layer('" + textLayer.name + "'); t.transform.anchorPoint;";
 shapeLayer.transform.position.setValue(textLayer.transform.anchorPoint.value);
-
-// Set the shape layer's transform properties to match the text layer
-shapeLayer.transform.scale.expression = textLayer.transform.scale.expression;
-shapeLayer.transform.rotation.expression = textLayer.transform.rotation.expression;
